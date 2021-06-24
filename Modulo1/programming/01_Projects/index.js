269a95e6cd7213ea3b92bfe4a3c4fddc307db24d
Kevin Chamorro <kevin_94ns@hotmail.com>
2021-06-24 12:03:54 -0500

    End ponit params query

--- Modulo1/programming/01_Projects/index.js
+++ Modulo1/programming/01_Projects/index.js
@@ -1,11 +1,32 @@
-const express = require('express');
+const express = require("express");
 //genera una aplicacion express
-const app =express()
+const app = express();
 //End point
-app.get('/',(req,res)=>{
-    res.send('Hoala mundo con ExpressJS');
-})
+app.get("/", (req, res) => {
+  res.send("Hoala mundo con ExpresJs");
+});
 
-app.listen(3000,()=>{
-    console.log("Servidor escuchando en http://localhost:3000");
-})
+app.get("/saludo", (req, res) => {
+  const {
+    query: { nombre, apellido },
+  } = req;
+  res.json({
+    saludo: "mundo",
+    nombre: nombre,
+    apellido: apellido,
+  });
+});
+
+app.get("/saludo/:nombre", (req, res) => {
+  const {
+    params: { nombre },
+  } = req;
+  res.json({
+    saludo: "mundo",
+    nombre: nombre,
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Servidor escuchando en http://localhost:3000");
+});
---
+++ Modulo1/programming/01_Projects/routes/index.js
@@ -0,0 +1,8 @@
+//traeos el paquete 
+const {Router} = require('express')
+//iniciazamos router
+const router = Router()
+//crecio  de los entpoint
+//Exportamos rutas
+module.exports.RouterIndex = router
+//middlewares con exprexs
