36d0b2d50f3bfac02258f91b2828783e165c7b94
Kevin Chamorro <kevin_94ns@hotmail.com>
2021-06-25 11:53:51 -0500

    Exportar rutas

--- Modulo1/programming/01_Projects/index.js
+++ Modulo1/programming/01_Projects/index.js
@@ -1,32 +1,12 @@
 const express = require("express");
 //genera una aplicacion express
 const app = express();
-//End point
-app.get("/", (req, res) => {
-  res.send("Hoala mundo con ExpresJs");
-});
-
-app.get("/saludo", (req, res) => {
-  const {
-    query: { nombre, apellido },
-  } = req;
-  res.json({
-    saludo: "mundo",
-    nombre: nombre,
-    apellido: apellido,
-  });
-});
-
-app.get("/saludo/:nombre", (req, res) => {
-  const {
-    params: { nombre },
-  } = req;
-  res.json({
-    saludo: "mundo",
-    nombre: nombre,
-  });
-});
-
+//routes
+const {RouterIndex}=require('./routes/index')
+app.use('/',RouterIndex)
+// app.use('/user',RouterIndex)
+// app.use('/user/data',RouterIndex)
+//app.use(RouterIndex)
 app.listen(3000, () => {
   console.log("Servidor escuchando en http://localhost:3000");
 });
--- Modulo1/programming/01_Projects/routes/index.js
+++ Modulo1/programming/01_Projects/routes/index.js
@@ -1,8 +1,39 @@
-//traeos el paquete 
-const {Router} = require('express')
+//traeos el paquete
+const { Router } = require("express");
 //iniciazamos router
-const router = Router()
-//crecio  de los entpoint
+const router = Router();
+//trtaer data
+const data =require('../data.json')
+//creacion  de los entpoint
+router
+    .get("/", (req, res) => {
+        res.send("Hoala mundo con ExpresJs");
+    })
+    .get("/users", (req, res) => {
+        res.json({
+            msg:"Lista de usuairios",
+            body:data
+        });
+    })
+    .get("/saludo", (req, res) => {
+        const {
+            query: { nombre, apellido },
+        } = req;
+        res.json({
+            saludo: "mundo",
+            nombre: nombre,
+            apellido: apellido,
+        });
+    })
+    .get("/saludo/:nombre", (req, res) => {
+        const {
+            params: { nombre },
+        } = req;
+        res.json({
+            saludo: "mundo",
+            nombre: nombre,
+        });
+    })
 //Exportamos rutas
-module.exports.RouterIndex = router
+module.exports.RouterIndex = router;
 //middlewares con exprexs
